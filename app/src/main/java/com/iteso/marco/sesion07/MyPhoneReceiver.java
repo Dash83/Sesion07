ccff6c55962a4ff8a8c9334dc6b39dc7bafaf49b
Marco Caballero <caballeromarco@gmail.com>
2015-03-04 21:22:32 -0600

    Added notifications

--- app/src/main/java/com/iteso/marco/sesion07/MyPhoneReceiver.java
+++ app/src/main/java/com/iteso/marco/sesion07/MyPhoneReceiver.java
@@ -1,9 +1,13 @@
 package com.iteso.marco.sesion07;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
@@ -24,7 +28,21 @@ public class MyPhoneReceiver extends BroadcastReceiver
             if(state.equals(TelephonyManager.EXTRA_STATE_RINGING))
             {
                 String phoneNumber = extras.get(TelephonyManager.EXTRA_INCOMING_NUMBER).toString();
-                Log.v("PHONE_STATE", "Incoming number=" + phoneNumber);
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+                mBuilder.setSmallIcon(R.drawable.bender);
+                mBuilder.setContentTitle("Notification alert! Click me!");
+                mBuilder.setContentText("This is an Android notification yo!");
+
+                Intent out = new Intent(context, MainActivity.class);
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                stackBuilder.addParentStack(MainActivity.class);
+
+                stackBuilder.addNextIntent(out);
+                PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+                mBuilder.setContentIntent(resultPendingIntent);
+
+                NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.notify(1, mBuilder.build());
             }
         }
     }
